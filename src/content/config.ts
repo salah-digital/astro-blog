99c62fb0c773f79f03e9849771f50d1a0875327c
salah-digital <salahhibrahim131@gmail.com>
2026-06-04 21:12:26 +0300

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,27 @@
 // src/content/config.ts
-import { z, defineCollection } from 'astro:content';
+import { SITE } from "@config";
+import { defineCollection, z } from "astro:content";
 
-// تعريف القالب الصارم للمقالات (لا يقبل السيرفر أي مقال لا يحتوي على هذه البيانات)
-const blogCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        date: z.string(),
-        category: z.string(),
-        description: z.string(),
-        draft: z.boolean().default(false), // للتحكم في إخفاء أو إظهار المقال
+const blog = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      author: z.string().default(SITE.author),
+      pubDatetime: z.date(),
+      modDatetime: z.date().optional(),
+      title: z.string(),
+      featured: z.boolean().optional(),
+      draft: z.boolean().default(false),
+      tags: z.array(z.string()).default(["others"]),
+      ogImage: image()
+        .refine(img => img.width >= 1200 && img.height >= 630, {
+          message: "OpenGraph image must be at least 1200 X 630 pixels!",
+        })
+        .or(z.string())
+        .optional(),
+      description: z.string(),
+      canonicalURL: z.string().optional(),
     }),
 });
 
-export const collections = {
-    'blog': blogCollection,
-};
+export const collections = { blog };
